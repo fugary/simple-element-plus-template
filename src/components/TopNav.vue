84e006555936eeb9fa6564254c0ed993db610b39
Gary Fu <xuebaiyouling@gmail.com>
2023-04-30 21:21:08 +0800

    基础element-plus模板项目

---
+++ src/components/TopNav.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-menu
+      mode="horizontal"
+      :ellipsis="false">
+    <el-menu-item index="0">SIMPLE EP</el-menu-item>
+    <div class="flex-grow" />
+    <el-sub-menu index="1">
+      <template #title>语言</template>
+      <el-menu-item index="2-1">中文</el-menu-item>
+      <el-menu-item index="2-2">英文</el-menu-item>
+    </el-sub-menu>
+    <el-sub-menu index="2">
+      <template #title>主题</template>
+      <el-menu-item index="2-1">默认</el-menu-item>
+      <el-menu-item index="2-2">黑色</el-menu-item>
+      <el-menu-item index="2-3">紫色</el-menu-item>
+    </el-sub-menu>
+    <el-sub-menu index="3">
+      <template #title>个人中心</template>
+      <el-menu-item index="2-4-1">个人资料</el-menu-item>
+      <el-menu-item index="2-4-2">关于</el-menu-item>
+      <el-menu-item index="2-4-3">退出</el-menu-item>
+    </el-sub-menu>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: "TopNav"
+}
+</script>
+
+<style scoped>
+.flex-grow {
+  flex-grow: 1;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import LeftMenu from "@/components/LeftMenu.vue";
+import TopNav from "@/components/TopNav.vue";
+</script>
+
+<template>
+  <el-container class="index-container">
+    <el-header>
+      <top-nav/>
+    </el-header>
+    <el-container>
+      <el-aside>
+        <left-menu/>
+      </el-aside>
+      <el-main>
+        <router-view/>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
